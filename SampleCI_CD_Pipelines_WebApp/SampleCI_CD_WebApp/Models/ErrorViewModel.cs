0f040499ccb2a550b6cef6d74af150bfe26562c1
Tejaswi Kuppili <tejaswi.kuppili@cognine.com>
2025-02-01 12:54:14 +0530

    Code Push

---
+++ SampleCI_CD_Pipelines_WebApp/SampleCI_CD_WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SampleCI_CD_WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
